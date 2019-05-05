be74af8192e4705f129f2faa8e20ba2f7d712796
NickGraus <nick-graus@hotmail.com>
2019-05-05 14:58:45 +0200

    Content added

---
+++ script/script1.js
@@ -0,0 +1,66 @@
+var xhr;
+var menuItem = document.getElementsByClassName("menuItem");
+var clickedMenuItem;
+var fileName;
+
+function setXhr() {
+  if (window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest();
+  } else if (window.ActiveXObject) {
+    xhr = new ActiveXObject("Msxml2.XMLHTTP");
+  } else {
+    throw new Error("Ajax is not supported by your browser");
+  }
+}
+
+function getMenuItem() {
+
+  for (i = 0; i < menuItem.length; i++) {
+    menuItem[i].onclick = function(e) {
+      clickedMenuItem = this.id;
+    }
+
+    menuItem[i].addEventListener('click', getMenuItem);
+  }
+
+  if (clickedMenuItem === "sco") {
+    fileName = "sco.html";
+    console.log(fileName);
+  } else if (clickedMenuItem === "dev") {
+    fileName = "dev.html";
+    console.log(fileName);
+  } else if (clickedMenuItem === "uex") {
+    fileName = "uex.html";
+    console.log(fileName);
+  } else if (clickedMenuItem === "ptm") {
+    fileName = "ptm.html";
+    console.log(fileName);
+  } else {
+     fileName = "index.html";
+   }
+
+  getData(fileName)
+}
+
+function getData(file) {
+  setXhr();
+  var fileName = file;
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState < 4)
+      document.getElementsByClassName("content").innerHTML = "Loading...";
+    else if (xhr.readyState === 4) {
+      if (xhr.status == 200 && xhr.status < 300) {
+        document.getElementById("content").innerHTML = xhr.responseText;
+      }
+    }
+  }
+  xhr.open('GET', 'content/' + fileName, true);
+  xhr.send(null);
+}
+
+function loadFunctions() {
+  getMenuItem();
+}
+
+
+document.onload = loadFunctions();
